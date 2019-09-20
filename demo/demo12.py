0b4d87c952e2073735ac8021554e3ce66451065f
Junenatte-and-little-pig <sy5622_5@126.com>
2019-09-20 10:04:03 +0800

    a BORING morning with nothing new

--- demo/demo12.py
+++ demo/demo12.py
@@ -28,6 +28,17 @@ def add6(*args, **kwargs):
     return sum(args), sum(kwargs.values())
 
 
+# 匿名函数 适用于简单代码
+add7 = lambda a, b: a + b
+
+
+def add8(a, b, lam):
+    """
+    :type lam:lambda expression
+    """
+    return a, b, lam(a, b)
+
+
 add1(1, 2)
 a = 1
 b = 2
@@ -43,3 +54,5 @@ print(add4(*(1, 2, 3, 4, 5)))
 print(add5(a1=12, a2=23, a3=1))
 print(add5(**{'a1': 12, 'a2': 23, 'a3': 1}))
 print(add6(*(1, 2, 3, 4, 5), **{'a1': 12, 'a2': 23, 'a3': 1}))
+print(add7(12, 20))
+print(add8(12, 32, lambda x, y: x + y))
